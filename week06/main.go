5baa393882e8dff1dd40bad212fc71fc0c650dd8
dhyes <dhyes0125@gmail.com>
2024-10-04 15:04:01 +0900

    v0.4 Unicodes

--- week06/main.go
+++ week06/main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	var i int = 13
 	f := 12.9
+	c1 := 'Z'
+	c2 := '김'
 	// fmt.Printf("%d* %f=%f\n", i, f, float64(i)*f)
 	fmt.Printf("%d* %f=%d\n", i, f, i*int(f))
-	fmt.Println(reflect.TypeOf(f))
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
