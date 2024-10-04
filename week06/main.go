483716bcc8131205710274cf3937b5a5d7f1f386
dhyes <dhyes0125@gmail.com>
2024-10-04 15:52:40 +0900

    v0.5 Zero Value

--- week06/main.go
+++ week06/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
-	c1 := 'Z'
-	c2 := '김'
-	// fmt.Printf("%d* %f=%f\n", i, f, float64(i)*f)
-	fmt.Printf("%d* %f=%d\n", i, f, i*int(f))
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	var f float64
+	var i int
+	var b bool
+	var s string
+
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f <= float64(i), "\n")
 }
